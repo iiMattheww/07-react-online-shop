c6f1cddda3677a72b77f98e3663e6e8835ff17d2
matin <150115043+iiMattheww@users.noreply.github.com>
2025-12-25 19:08:07 +0330

    Update shopping-cart-context.jsx

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -67,38 +67,4 @@ function shoppingCartReducer(state, action) {
     return state;
 }
 
-export default function CartContextProvider({ children }) {
-    const [shoppingCartState, shoppingCartDispatch] = useReducer(
-        shoppingCartReducer,
-        {
-            items: [],
-        }
-    );
 
-    function handleAddItemToCart(id) {
-        shoppingCartDispatch({
-            type: "ADD_ITEM",
-            payload: id,
-        });
-    }
-
-    function handleUpdateCartItemQuantity(productId, amount) {
-        shoppingCartDispatch({
-            type: "UPDATE_ITEM",
-            payload: {
-                productId,
-                amount,
-            },
-        });
-    }
-
-    const ctxValue = {
-        items: shoppingCartState.items,
-        addItemToCart: handleAddItemToCart,
-        updateItemQuantity: handleUpdateCartItemQuantity,
-    };
-
-    return (
-        <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
-    );
-}
